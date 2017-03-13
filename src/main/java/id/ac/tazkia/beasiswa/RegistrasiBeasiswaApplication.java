5dc6df4c1f3e80d6c7d37787e4f0861ce27168ab
Endy Muhardin <endy.muhardin@gmail.com>
2017-03-13 11:15:33 +0700

    starter project

---
+++ src/main/java/id/ac/tazkia/beasiswa/RegistrasiBeasiswaApplication.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.beasiswa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrasiBeasiswaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrasiBeasiswaApplication.class, args);
+	}
+}
